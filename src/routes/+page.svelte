75b282b29dc9774b8625c03a2375d4386a93d6a1
ryosuke-horie <ryosuke03140@gmial.com>
2023-09-03 01:23:20 +0900

    インクリメントするボタンを実装

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,24 @@
 <script>
 	let name = 'Svelte';
+
+    // カウントボタン
+	let count = 0;
+
+	function increment() {
+		count += 1;
+	}
 </script>
 
 <h1>Hello {name.toUpperCase()}!</h1>
 
 
 <p>This is a paragraph.</p>
+
+<button on:click={increment}>
+	Clicked {count}
+	{count === 1 ? 'time' : 'times'}
+</button>
+
 
 <style>
 	p {
@@ -14,3 +27,4 @@
 		font-size: 2em;
 	}
 </style>
+
