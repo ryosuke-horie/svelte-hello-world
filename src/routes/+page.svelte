346293448565e1267c8aaf4c97a2b5876ad9c1c8
ryosuke-horie <ryosuke03140@gmial.com>
2023-09-03 01:19:43 +0900

    スタイリング

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,3 +3,14 @@
 </script>
 
 <h1>Hello {name.toUpperCase()}!</h1>
+
+
+<p>This is a paragraph.</p>
+
+<style>
+	p {
+		color: goldenrod;
+		font-family: 'Comic Sans MS', cursive;
+		font-size: 2em;
+	}
+</style>
